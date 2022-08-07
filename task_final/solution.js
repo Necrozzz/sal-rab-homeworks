b5853cb0173c732045d48d5c4505e21e4330a657
Roman Doronin <doroninroman@gmail.com>
2022-08-07 16:45:30 +0400

    Take 3

--- task_final/solution.js
+++ task_final/solution.js
@@ -44,7 +44,7 @@ function sendRequest(name, phone, address, goods, sum) {
         data.goods.push(goods[i].title);
     }
     
-    data.order.address = `ул. ${street}, дом ${house}, ${entrance} подъезд, ${floor} этаж, кв. ${flat}`;
+    data.order.address = `ул. ${street}, дом ${house}, ${entrance} подъезд, ${floor} этаж, кв ${flat}`;
     data.order.sum = sum;
     data.client = `${name}, ${phone}`;
 
