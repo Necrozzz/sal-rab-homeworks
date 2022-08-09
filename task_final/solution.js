634b2a2ee682ce6f8995c9c05dac60c6be51ac3a
Roman Doronin <doroninroman@gmail.com>
2022-08-09 09:49:48 +0400

    Take 4

--- task_final/solution.js
+++ task_final/solution.js
@@ -40,8 +40,13 @@ function sendRequest(name, phone, address, goods, sum) {
     let countOfGoods = goods.length;
 
     for (let i = 0; i < countOfGoods; i += 1) {
-        goods = {title: {}, count: {}};
-        data.goods.push(goods[i].title);
+        goods1 = [
+            {
+                "title": "Пицца", 
+                "count": 2
+            }
+        ];
+        data.goods.push(goods1[i]);
     }
     
     data.order.address = `ул. ${street}, дом ${house}, ${entrance} подъезд, ${floor} этаж, кв ${flat}`;
